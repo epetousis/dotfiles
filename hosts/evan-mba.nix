7e90d014a0288d3393c866997209996ec120465d
Evan Petousis <evan@petousis.net>
2022-08-24 23:45:13 +1000

    feat(hosts/evan-mba): add Homebrew options

--- hosts/evan-mba.nix
+++ hosts/evan-mba.nix
@@ -33,6 +33,80 @@
   programs.zsh.enable = true;  # default shell on catalina
   # programs.fish.enable = true;
 
+  homebrew = {
+    enable = true;
+    cleanup = "zap";
+    taps = [
+      "homebrew/cask"
+      "homebrew/cask-versions"
+      "homebrew/cask-fonts"
+      "homebrew/cask-drivers"
+    ];
+    casks = [
+      "1password"
+      "altserver"
+      "amethyst"
+      "anki"
+      "balenaetcher"
+      "barrier"
+      "betterdummy"
+      "chatterino"
+      "coconutbattery"
+      "cryptomator"
+      "cyberduck"
+      "discord"
+      "displaylink"
+      "displaylink-login-extension"
+      "dosbox-x"
+      "element"
+      "emacs"
+      "firefox"
+      "font-fira-code"
+      "font-fira-code-nerd-font"
+      "google-chrome"
+      "graphiql"
+      "hot"
+      "iina"
+      "istat-menus"
+      "iterm2-beta"
+      "keepingyouawake"
+      "maccy"
+      "macfuse"
+      "mailtrackerblocker"
+      "minecraft"
+      "mirrorop"
+      "mixxx"
+      "monitorcontrol"
+      "ngrok"
+      "notion"
+      "obs"
+      "openemu"
+      "parsec"
+      "paw"
+      "plex"
+      "protonvpn"
+      "pycharm"
+      "rectangle"
+      "rekordbox"
+      "runelite"
+      "sf-symbols"
+      "signal"
+      "soulseek"
+      "spotify"
+      "sublime-text"
+      "syncthing"
+      "the-unarchiver"
+      "touchosc-bridge"
+      "transmission"
+      "utm"
+      "vimr"
+      "visual-studio-code"
+      "webstorm"
+      "yubico-yubikey-manager"
+      "zoom"
+    ];
+  };
+
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
