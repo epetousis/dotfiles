0cd2b2043d7c848aabc40270bc22d5398a66c247
Evan Petousis <evan@petousis.net>
2022-08-26 17:32:04 +1000

    feat(configs/emacs): switch to use-package, add lsp-mode

--- configs/emacs/.emacs.d/init.el
+++ configs/emacs/.emacs.d/init.el
@@ -2,33 +2,55 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-(package-refresh-contents)
+(package-refresh-contents t)
 
-;; Download Evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
-;; Download undo-fu (remove with Emacs 28)
-(package-install 'undo-fu)
+;; This is only needed once, near the top of the file
+(eval-when-compile
+  (require 'use-package))
 
 ;; Install packages
-(unless (package-installed-p 'fzf)
-  (package-install 'fzf))
-(unless (package-installed-p 'evil-collection)
-  (package-install 'evil-collection))
+(use-package fzf :ensure t)
+
+;; LSP
+(use-package lsp-mode :ensure t
+  :hook (
+    (vue-mode . lsp-deferred)
+    (typescript-mode . lsp-deferred))
+  :commands (lsp lsp-deferred))
+(use-package lsp-ui :after lsp-mode :ensure t
+  :commands lsp-ui-mode)
+(use-package company :after lsp-mode :ensure t)
 
 ;; Evil mode related settings
-(setq evil-undo-system 'undo-fu)
+; Enable evil mode
 ;; `evil-collection' assumes `evil-want-keybinding' is set to
 ;; `nil' before loading `evil' and `evil-collection'
 ;; @see https://github.com/emacs-evil/evil-collection#installation
-(setq evil-want-keybinding nil)
-;; Enable Evil mode
-(require 'evil)
-(evil-mode 1)
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-keybinding nil)
+  (setq evil-undo-system 'undo-redo)
+  :config
+  (evil-mode 1)
+  ;; Change evil's initial state in term mode to emacs (zsh should be in vi-mode anyway)
+  (evil-set-initial-state 'term-mode 'emacs)
+  ;; Define keybinds in evil
+  (evil-define-key nil 'global (kbd "C-j") 'next-buffer)
+  (evil-define-key nil 'global (kbd "C-k") 'previous-buffer)
+  (define-key evil-normal-state-map (kbd "g h") 'lsp-ui-doc-glance))
+(use-package evil-collection :ensure t
+  :after evil
+  :config
+  ;; Register evil-collection bindings
+  (evil-collection-init))
 
-;; Register evil-collection bindings
-(evil-collection-init)
+(use-package which-key :ensure t
+  :config
+  (which-key-mode))
 
 ;; Close FZF with esc (https://github.com/bling/fzf.el/issues/45#issuecomment-429893494)
 (require 'term)
@@ -39,12 +61,6 @@
 ;; to quit fzf with ESC key
 (define-key term-raw-map (kbd "<escape>") 'term-send-esc)
 
-;; Change evil's initial state in term mode to emacs (zsh should be in vi-mode anyway)
-(evil-set-initial-state 'term-mode 'emacs)
-
-(evil-define-key nil 'global (kbd "C-j") 'next-buffer)
-(evil-define-key nil 'global (kbd "C-k") 'previous-buffer)
-
 ;; Move all autosaves to a directory
 (setq autosave-dir "~/.emacs-saves/")
 (unless (file-exists-p autosave-dir)
@@ -52,8 +68,16 @@
 (setq auto-save-file-name-transforms
   `((".*" ,autosave-dir t)))
 
-;; Move custom vars to separate file - create if needed
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(unless (file-exists-p custom-file)
-  (write-region "" nil custom-file))
-(load custom-file)
+;; Language specific major modes
+(use-package vue-mode :ensure t :mode "\\.vue\\'"
+  ;; Disable that fugly mmm-mode background default
+  :init
+  (setq mmm-submode-decoration-level 0))
+(use-package typescript-mode :ensure t :mode "\\.ts\\'")
+
+;; direnv integration - allows us to easily use Nix packages
+;; Place this late in the startup since minor modes prepend themselves to hooks
+(use-package envrc
+  :ensure t
+  :config
+  (envrc-global-mode))
