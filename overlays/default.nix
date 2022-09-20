760ca244d644f6956546a33ce523a1f11d723a47
Evan Petousis <evan@petousis.net>
2022-09-20 18:43:03 +1000

    fix(overlays): using prev.callPackage

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,15 +1,15 @@
 final: prev: {
-  discord = if prev.stdenv.hostPlatform.isLinux then prev.callPackage ./discord { discord = prev.discord; } else prev.discord;
+  discord = if prev.stdenv.hostPlatform.isLinux then final.callPackage ./discord { discord = prev.discord; } else prev.discord;
 
-  yabai = prev.callPackage ./yabai { yabai = prev.yabai; };
+  yabai = final.callPackage ./yabai { yabai = prev.yabai; };
 
-  spotify = if prev.stdenv.hostPlatform.isLinux then prev.callPackage ./spotify { spotify = prev.spotify; } else prev.spotify;
+  spotify = if prev.stdenv.hostPlatform.isLinux then final.callPackage ./spotify { spotify = prev.spotify; } else prev.spotify;
 
-  discord-screenaudio = prev.callPackage ./discord-screenaudio {};
+  discord-screenaudio = final.callPackage ./discord-screenaudio {};
 
-  webcord = prev.callPackage ./webcord {};
+  webcord = final.callPackage ./webcord {};
 
-  webcord-keybinds = prev.callPackage ./webcord/keybinds.nix { webcord = final.webcord; };
+  webcord-keybinds = final.callPackage ./webcord/keybinds.nix { webcord = final.webcord; };
 
-  mpv = prev.callPackage ./mpv { mpv = prev.mpv; };
+  mpv = final.callPackage ./mpv { mpv = prev.mpv; };
 }
