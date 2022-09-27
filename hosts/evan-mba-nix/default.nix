0661fadc09a2dc92e388bcd4754ae77b28465517
Evan Petousis <evan@petousis.net>
2022-09-27 14:45:40 +1000

    feat(hosts/evan-mba-nix): open port 8080

--- hosts/evan-mba-nix/default.nix
+++ hosts/evan-mba-nix/default.nix
@@ -114,7 +114,7 @@
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 8080 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
