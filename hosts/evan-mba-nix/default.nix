e0ab77397e37562e069a406065a1802da79adbec
Evan Petousis <evan@petousis.net>
2022-10-08 18:05:03 +1100

    feat(hosts/evan-mba-nix): add appindicator extension

--- hosts/evan-mba-nix/default.nix
+++ hosts/evan-mba-nix/default.nix
@@ -106,6 +106,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #   wget
     gnomeExtensions.gsconnect
+    gnomeExtensions.appindicator
   ];
 
   # Enable iOS USB support
