5c1ce4911d0fa620018f6693172dc4430601594f
Evan Petousis <evan@petousis.net>
2022-10-28 12:52:44 +1100

    feat(modules/home): add poetry support to direnv

--- modules/home.nix
+++ modules/home.nix
@@ -181,8 +181,29 @@
   };
 
   # Add direnv support - among other things, this can be used for automatically loading shell.nix files
-  programs.direnv.enable = true;
-  programs.direnv.nix-direnv.enable = true;
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+    # Add layout_poetry from https://github.com/direnv/direnv/issues/592#issuecomment-856227234
+    stdlib = ''
+      layout_poetry() {
+        if [[ ! -f pyproject.toml ]]; then
+          log_error 'No pyproject.toml found. Use `poetry new` or `poetry init` to create one first.'
+          exit 2
+        fi
+
+        local VENV=$(poetry env info --path)
+        if [[ -z $VENV || ! -d $VENV/bin ]]; then
+          log_error 'No poetry virtual environment found. Use `poetry install` to create one first.'
+          exit 2
+        fi
+
+        export VIRTUAL_ENV=$VENV
+        export POETRY_ACTIVE=1
+        PATH_add "$VENV/bin"
+      }
+    '';
+  };
 
   # Symlink macOS apps - taken from https://github.com/nix-community/home-manager/issues/1341#issuecomment-1190875080
   home.activation = lib.mkIf pkgs.stdenv.isDarwin {
