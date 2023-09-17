c708e3beb3f5b06af631b0cfd97db1549b56ff64
Evan Petousis <evan@petousis.net>
2023-09-17 17:18:54 +1000

    modules/home: add ctrl-x-e to zsh

--- modules/home.nix
+++ modules/home.nix
@@ -130,6 +130,12 @@
     bindkey "^H" backward-delete-char
     bindkey "^?" backward-delete-char
 
+    # Enable Ctrl-x-e for CLI editing
+    autoload -U edit-command-line
+    zle -N edit-command-line
+    bindkey '^xe' edit-command-line
+    bindkey '^x^e' edit-command-line
+
     eval "$(direnv hook zsh)"
     '';
   };
