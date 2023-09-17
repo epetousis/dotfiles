f008c5d1bbb88a388c232539a2a90a24c5f8e254
Evan Petousis <evan@petousis.net>
2023-09-17 17:19:07 +1000

    modules/home: use zsh for nix-shell

--- modules/home.nix
+++ modules/home.nix
@@ -99,6 +99,16 @@
     enable = true;
     plugins = [
       {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.7.0";
+          sha256 = "149zh2rm59blr2q458a5irkfh82y3dwdich60s9670kl3cl5h2m1";
+        };
+      }
+      {
         file = "powerlevel10k.zsh-theme";
         name = "powerlevel10k";
         src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k";
