4140bdb8d0ee67b2ca4568fca4867fb41dc2e8de
Evan Petousis <evan@petousis.net>
2024-03-02 18:45:01 +1100

    evan-pc: fix efi mountpoint

--- hosts/evan-pc/hardware-configuration.nix
+++ hosts/evan-pc/hardware-configuration.nix
@@ -21,7 +21,7 @@
               content = {
                 type = "filesystem";
                 format = "vfat";
-                mountpoint = "/boot";
+                mountpoint = "/boot/efi";
               };
             };
             root = {
