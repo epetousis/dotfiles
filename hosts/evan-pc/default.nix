0f36b7cce2368743ea5d90b55d9127df93338de9
Evan Petousis <evan@petousis.net>
2024-03-02 18:55:12 +1100

    evan-pc: fix systemd-boot's EFI mountpoint

--- hosts/evan-pc/default.nix
+++ hosts/evan-pc/default.nix
@@ -13,7 +13,6 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
   # Add custom kernel modules
   boot.extraModulePackages = with config.boot.kernelPackages; [
