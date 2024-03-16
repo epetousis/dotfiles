4248811f62ab0dc2ab6ee2a1e2a027dca622fd78
Evan Petousis <evan@petousis.net>
2024-03-16 16:15:30 +1100

    evan-mba: fix missing symlink module

--- hosts/evan-mba.nix
+++ hosts/evan-mba.nix
@@ -15,6 +15,8 @@
       pkgs.slack
     ];
 
+  system.symlinkApps.enable = true;
+
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
   # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
