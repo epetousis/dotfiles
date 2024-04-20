95bedab5b9a0f2a271ceb986150a282ecfac08a3
Evan Petousis <evan@petousis.net>
2024-04-20 15:30:54 +1000

    evan-pc: add podman-compose

--- hosts/evan-pc/default.nix
+++ hosts/evan-pc/default.nix
@@ -138,6 +138,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
     openai-whisper-cpp
+    podman-compose
   ];
 
   # Services:
