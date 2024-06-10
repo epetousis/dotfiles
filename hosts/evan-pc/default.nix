b65993ed381149ff895bdd6977337da09ee91fb5
Evan Petousis <evan@petousis.net>
2024-06-10 12:26:30 +1000

    evan-pc: enable adb

--- hosts/evan-pc/default.nix
+++ hosts/evan-pc/default.nix
@@ -109,6 +109,9 @@
     openFirewall = true;
   };
 
+  # Enable adb.
+  programs.adb.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
