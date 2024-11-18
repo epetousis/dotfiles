795217515fea72ee9ed0db9c0c4f014b2c965e62
Evan Petousis <evan@petousis.net>
2024-11-18 21:03:59 +1100

    home: don't assume `switch` subcommand for nxrb

--- modules/home.nix
+++ modules/home.nix
@@ -131,7 +131,7 @@
 
     alias nxrb='${if pkgs.stdenv.hostPlatform.isDarwin
                   then "darwin-rebuild"
-                  else "${pkgs.systemd}/bin/run0 --setenv=PATH=${pkgs.git}/bin:${pkgs.systemd}/bin/ nixos-rebuild"} switch --flake ~/.local/share/dotfiles'
+                  else "${pkgs.systemd}/bin/run0 --setenv=PATH=${pkgs.git}/bin:${pkgs.systemd}/bin/ nixos-rebuild"} --flake ~/.local/share/dotfiles'
 
     alias e='emacsclient -r --no-wait'
 
